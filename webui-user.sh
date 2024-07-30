2c78103d6e22d94e49b737d53db58d5238b68b54
Anh Khoa <dev.nakhoa@gmail.com>
2024-07-30 16:32:05 +0700

    Up

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--api --api-auth seatek:Seatek@123 --xformers --listen"
+export COMMANDLINE_ARGS="--api --xformers --listen"
 
 # python3 executable
 #python_cmd="python3"
